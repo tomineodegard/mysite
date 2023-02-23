4c9bc2a91aed23afe26ebc53484b26dd1826ddd9
tomineodegard <tomi0105@stud.kea.dk>
2023-02-23 15:43:44 +0100

    testing

--- app.py
+++ app.py
@@ -28,10 +28,8 @@ def render_index():
         db = sqlite3.connect(str(pathlib.Path(__file__).parent.resolve())+"/twitter.db")
         db.row_factory = dict_factory
         tweets = db.execute("SELECT * FROM tweets JOIN users ON tweets.user_fk = users.user_id").fetchall()
-        trends = db.execute(
-            "SELECT * FROM tweets JOIN users ON tweets.user_fk = users.user_id").fetchall()
-        return "ok3"
-        # return template("index", title="Twitter", trends=trends, tweets=tweets, people=people)
+        trends = db.execute("SELECT * FROM tweets JOIN users ON tweets.user_fk = users.user_id").fetchall()
+        return template("index", title="Twitter", trends=trends, tweets=tweets, people=people)
         
 
     except Exception as ex:
